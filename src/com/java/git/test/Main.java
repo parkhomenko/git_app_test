eb5b14b38c56117b4b061422dd15ae58717cb239
Stanislav Parkhomenko <sparkhomenko@lohika.com>
2016-04-04 17:05:24 +0300

    something to commit

--- src/com/java/git/test/Main.java
+++ src/com/java/git/test/Main.java
@@ -3,14 +3,48 @@ package com.java.git.test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        int[] array = new int[101];
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        array[100] = 5050;
+
+        long iterDuration = checkIteratively();
+        System.out.println("It took " + iterDuration);
+
+        long formulaDuration = checkByFormula();
+        System.out.println("It took " + formulaDuration);
+
+        long start = System.nanoTime();
+        int sum = array[100];
+        long end = System.nanoTime();
+        System.out.println("It took " + (end - start));
+    }
+
+    public static long checkIteratively() {
+        int sum = 0;
+        long start = System.nanoTime();
+
+        for (int i = 1; i <= 1000; i++) {
+            sum += i;
         }
 
-        System.out.println("New line");
-        System.out.println("New line");
-        System.out.println("New line");
+        long end = System.nanoTime();
+        long duration = end - start;
+
+        System.out.println(sum);
+
+        return duration;
+    }
+
+    public static long checkByFormula() {
+        int sum = 0;
+        long start = System.nanoTime();
+
+        sum = (1 + 1000) * 1000 / 2;
+
+        long end = System.nanoTime();
+
+        System.out.println(sum);
+
+        return end - start;
     }
 }
